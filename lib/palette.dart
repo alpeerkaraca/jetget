ba36e7a2c15aa7aef4210200d5c7e6a527db98d5
Alper Karaca <74460407+alpeerkaraca@users.noreply.github.com>
2024-01-10 19:58:59 +0300

    Palette eklendi

---
+++ lib/palette.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+   Color black =  const Color(0xFF212121);
+   Color darkAqua = const Color(0xFF005F73);
+   Color aqua = const Color(0xFF0A9396);
+   Color lightAqua = const Color(0xFF94D2BD);
+   Color bg = const Color(0xFFE9D8A6);
+   Color orange = const Color(0xFFEE9B00);
+   Color deepOrange = const Color(0xFFCA6702);
+   Color darkOrange = const Color(0xFFBB3E03);
+   Color red = const Color(0xFFAE2012);
+   Color darkRed = const Color(0xFF9B2226);
+   ColorPalette();
+
+}
+
